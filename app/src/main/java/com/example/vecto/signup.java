9e34a6535f82d42ed2ab72e3fc4c5e9d82dfbcb4
VEADER <sj.sahiljain1005@gmail.com>
2022-09-23 16:07:52 +0530

    signup page layout created

---
+++ app/src/main/java/com/example/vecto/signup.java
@@ -0,0 +1,27 @@
+package com.example.vecto;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.vecto.databinding.ActivityLoginPageBinding;
+
+public class signup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        getSupportActionBar().hide();
+
+    }
+}
