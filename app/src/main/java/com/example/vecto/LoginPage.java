b72b96aa683fb63995e7496766b508c6403818d6
VEADER <sj.sahiljain1005@gmail.com>
2022-09-30 14:42:27 +0530

    fixed a few bugs

--- app/src/main/java/com/example/vecto/LoginPage.java
+++ app/src/main/java/com/example/vecto/LoginPage.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.navigation.NavController;
@@ -18,24 +19,38 @@ import androidx.navigation.ui.NavigationUI;
 
 public class LoginPage extends AppCompatActivity {
     private TextView su;
+    private Button log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         su =(TextView) findViewById(R.id.opensignup);
+        log_in = (Button) findViewById(R.id.login);
+
+
 
         su.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSignup();
             }
-
         });
 
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openHomePage();
+            }
+        });
     }
     private void openSignup() {
         Intent i = new Intent(LoginPage.this, signup.class);
         startActivity(i);
+    }
+
+    private void openHomePage() {
+        Intent j = new Intent(LoginPage.this, HomePage.class);
+        startActivity(j);
     }
 }
