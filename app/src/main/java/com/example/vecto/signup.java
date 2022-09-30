6b2a13b4e5e18f19ef40cfdcfc23b927c9a25ba8
Harshit228 <info.harshit28@gmail.com>
2022-09-30 14:47:49 +0530

    home page linked to signup page

--- app/src/main/java/com/example/vecto/signup.java
+++ app/src/main/java/com/example/vecto/signup.java
@@ -7,6 +7,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -15,11 +17,27 @@ import androidx.navigation.ui.NavigationUI;
 
 public class signup extends AppCompatActivity {
 
+
+    private Button su;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        getSupportActionBar().hide();
 
+        su = (Button) findViewById(R.id.btn_homepage);
+
+        su.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openhomepage();
+            }
+
+
+        });
     }
+        private void openhomepage() {
+            Intent i = new Intent(signup.this, HomePage.class);
+            startActivity(i);
+        }
 }
