a7f3d42744ffa931d8d4118cd94fc63b15f6433c
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2022-10-01 13:17:23 +0530

    drop down menus added

--- app/src/main/java/com/example/vecto/create_request.java
+++ app/src/main/java/com/example/vecto/create_request.java
@@ -4,10 +4,14 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
@@ -15,6 +19,8 @@ public class create_request extends AppCompatActivity {
 
     TextView tv;
     int t1hour, t1Minute;
+    TextInputLayout textInputLayout, textInputLayout2;
+    AutoCompleteTextView autoCompleteTextView, autoCompleteTextView2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,5 +50,17 @@ public class create_request extends AppCompatActivity {
 
             }
         });
+        textInputLayout = findViewById(R.id.menu1);
+        autoCompleteTextView = findViewById(R.id.drop_items1);
+        String[] items1 = {"Male","Female","Any"};
+        ArrayAdapter<String> itemAdapter=new ArrayAdapter<>(create_request.this, R.layout.items_list,items1);
+        autoCompleteTextView.setAdapter(itemAdapter);
+
+        textInputLayout2 = findViewById(R.id.menu2);
+        autoCompleteTextView2 = findViewById(R.id.drop_items2);
+        String[] items2 = {"0","1","2","3","4","5"};
+        ArrayAdapter<String> itemAdapter2 = new ArrayAdapter<>(create_request.this, R.layout.items_list, items2);
+        autoCompleteTextView2.setAdapter(itemAdapter2);
+
     }
 }
